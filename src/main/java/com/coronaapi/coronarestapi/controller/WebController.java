d9a15fd195502af547b57312762bf6fc5169d216
Muhib Shaikh <muhib.shaikh313@gmail.com>
2020-03-28 00:27:08 +0530

    update

--- src/main/java/com/coronaapi/coronarestapi/controller/WebController.java
+++ src/main/java/com/coronaapi/coronarestapi/controller/WebController.java
@@ -1,15 +1,14 @@
 package com.coronaapi.coronarestapi.controller;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.Dictionary;
-import java.util.Hashtable;
 
+import java.util.ArrayList;
+
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomNodeList;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlTable;
 import com.gargoylesoftware.htmlunit.html.HtmlTableCell;
@@ -20,10 +19,12 @@ import com.coronaapi.coronarestapi.models.SampleResponse;
 
 @RestController
 public class WebController {
+	
+	@Scheduled(cron = "* 1 * * * *")
 	@RequestMapping("/sample")
-	public SampleResponse Sample(@RequestParam(value = "name",
-	defaultValue = "Robot") String name) throws Exception {
+	public ArrayList<SampleResponse> Sample() throws Exception {
 		
+
 		
 		WebClient client = new WebClient();
 		client.getOptions().setCssEnabled(false);
@@ -32,46 +33,23 @@ public class WebController {
 		final String url = "https://www.mohfw.gov.in/";
 		HtmlPage page = client.getPage(url);
 
-//    	final HtmlTable table = page.getHtmlElementById("table table-striped table-dark");
-
-		final HtmlTable table = (HtmlTable) page.getByXPath("//table[@class='table table-striped table-dark']").get(1);
-
-		Dictionary dictionary = new Hashtable();
-		int i=0;
-		for (final HtmlTableRow row : table.getRows()) {
-
-			if(row.asText().contains("S. No.")) {
-    	    System.out.println(row.asText());
-		}
-		else if(row.asText().contains("Name of State")) {
-			System.out.println(row.asText());
-		}
-		else if(row.asText().contains("Total Confirmed cases (Indian National)")) {
-			System.out.println(row.asText());
-		}
-		i++;
-		System.out.println(row.asText());
-
-
-//			for (final HtmlTableCell cell : row.getCells()) {
-//				if(cell.asText().contains("S. No.")) {
-//		    	    System.out.println(cell.asText());
-//				}
-//				else if(cell.asText().contains("Name of State")) {
-//					System.out.println(cell.asText());
-//				}
-//				else if(cell.asText().contains("Total Confirmed cases (Indian National)")) {
-//					System.out.println(cell.asText());
-//				}
-//				i++;
-//				System.out.println(i);
-//			}
-
-		}		
 		
-		SampleResponse response = new SampleResponse();
-		response.setId(1);
-		response.setMessage("Your name is "+name);
-		return response;
+		final HtmlTable table = (HtmlTable) page.getByXPath("//table[@class='table table-striped table-dark']").get(7);
+		
+		DomNodeList<HtmlElement> cell = table.getElementsByTagName("td");
+
+		
+
+		
+		ArrayList<SampleResponse> arrayList = new ArrayList<SampleResponse>();
+
+		for(int i = 1 ; i<28 ; i++) {
+			for(int j=1;j<2;j++) {
+				arrayList.add(new SampleResponse(table.getCellAt(i, j).asText(), table.getCellAt(i, j+1).asText(), table.getCellAt(i, j+2).asText(), table.getCellAt(i, j+3).asText(), table.getCellAt(i, j+4).asText()));
+			}
+		}
+		
+		System.out.println("running....");
+		return arrayList ;
 	}
 }
--- src/main/java/com/coronaapi/coronarestapi/models/SampleResponse.java
+++ src/main/java/com/coronaapi/coronarestapi/models/SampleResponse.java
@@ -1,22 +1,55 @@
 package com.coronaapi.coronarestapi.models;
 
+import java.util.Collection;
+
 public class SampleResponse {
-	private String message;
-	private long id;
-
-	public String getMessage() {
-		return message;
+	private String state;
+	private String totalConfirmedCasesIndian;
+	private String totalConfirmedCasesForeign;
+	private String cured;
+	private String death;
+	
+	
+	public SampleResponse(String state, String totalConfirmedCasesIndian, String totalConfirmedCasesForeign,
+			String cured, String death) {
+		super();
+		this.state = state;
+		this.totalConfirmedCasesIndian = totalConfirmedCasesIndian;
+		this.totalConfirmedCasesForeign = totalConfirmedCasesForeign;
+		this.cured = cured;
+		this.death = death;
 	}
-
-	public void setMessage(String message) {
-		this.message = message;
+	public String getState() {
+		return state;
 	}
-
-	public long getId() {
-		return id;
+	public void setState(String state) {
+		this.state = state;
 	}
-
-	public void setId(long id) {
-		this.id = id;
+	public String getTotalConfirmedCasesIndian() {
+		return totalConfirmedCasesIndian;
 	}
+	public void setTotalConfirmedCasesIndian(String totalConfirmedCasesIndian) {
+		this.totalConfirmedCasesIndian = totalConfirmedCasesIndian;
+	}
+	public String getTotalConfirmedCasesForeign() {
+		return totalConfirmedCasesForeign;
+	}
+	public void setTotalConfirmedCasesForeign(String totalConfirmedCasesForeign) {
+		this.totalConfirmedCasesForeign = totalConfirmedCasesForeign;
+	}
+	public String getCured() {
+		return cured;
+	}
+	public void setCured(String cured) {
+		this.cured = cured;
+	}
+	public String getDeath() {
+		return death;
+	}
+	public void setDeath(String death) {
+		this.death = death;
+	}
+	
+	
+	
 }
