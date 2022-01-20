4e1cfacd927f86a350be04ddb8dde7081465e7ad
itsDevShaikh <its.dev.shaikh@gmail.com>
2022-01-20 16:46:44 +0530

    Saperated api url for different locations

--- src/main/java/com/coronaapi/coronarestapi/CoronarestapiApplication.java
+++ src/main/java/com/coronaapi/coronarestapi/CoronarestapiApplication.java
@@ -9,8 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class CoronarestapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoronarestapiApplication.class, args);
-		
+		SpringApplication.run(CoronarestapiApplication.class, args);	
 	}
 
 }
